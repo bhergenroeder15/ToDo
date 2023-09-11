600cc1b5b6060e1b93c02341f13c7911cfd9c135
Benjamin Hergenroeder <benjamin.hergenroeder@comcast.net>
2023-09-11 17:58:00 -0400

    Added notes collapsable

--- Express/src/components/TaskList.tsx
+++ Express/src/components/TaskList.tsx
@@ -5,7 +5,7 @@ export default function TaskList(props : TaskListProps){
     return (
         <ul className="list-group">
             {props.list.length ? props.list.sort((a, b) => Date.parse(a.dueDate) - Date.parse(b.dueDate)).map((task: Task): ReactNode => {
-                return <li key={task.id} className={`list-group-item ${task.completed ? "list-group-item-success": Date.parse(task.dueDate) < Date.now() ? "list-group-item-danger" : task.priority ? "list-group-item-warning" : "list-group-item-info"}`}>
+                return <li key={task._id} className={`list-group-item ${task.completed ? "list-group-item-success": Date.parse(task.dueDate) < Date.now() ? "list-group-item-danger" : task.priority ? "list-group-item-warning" : "list-group-item-info"}`}>
                     <div>
                         <p><strong>Task:</strong> {task.taskName}</p>
                         <p><strong>Due Date: </strong>{new Date(task.dueDate).toLocaleString()}</p>
@@ -13,6 +13,10 @@ export default function TaskList(props : TaskListProps){
                     <div>
                         {task.priority ? <p><strong>!!!</strong></p> : null}
                         {Date.parse(task.dueDate) < Date.now() ? <p><strong>OVERDUE</strong></p> : null}
+                    </div>
+                    <button data-toggle="collapse" data-target={`#${task._id}notes`}>Details</button>
+                    <div id={`${task._id}notes`} className="collapse">
+                    {task.notes || "No notes added"}
                     </div>
                 </li>
             }) : <li className="list-group-item list-group-item-success">Nothing Here!</li>}
